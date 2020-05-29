c63f3ace977576feb912c09c2422f2b1b252fcf4
alejandrolozanoz <alejandrolozanoz96@gmail.com>
2020-05-29 10:27:51 -0500

    Create add and get methods for FunctionsTable and VariableTable and create Quadruple class

--- functions_table.py
+++ functions_table.py
@@ -1,6 +1,25 @@
 from antlr4 import *
 from function import Function
+from variable import Variable
 
 class FunctionsTable:
-    def __init__(self):
-        self.functions_table = {}
+	def __init__(self):
+		self.functions = {}
+
+	def add_function(self, function_type, function_name, function_parameters):
+		if function_name in self.functions:
+			raise Exception("{} was already defined in the functions table".format(function_name))
+
+		else:
+			self.functions[function_name] = Function(function_name, function_type, {})
+			self.functions[function_name].function_parameters = function_parameters
+			for i in range(len(function_parameters)):
+				self.functions[function_name].function_parameters[i] = Variable(
+					function_parameters[i].type, function_parameters[i].name, function_parameters[i].value)
+
+	def get_function(self, function_name):
+		if function_name not in self.functions:
+			raise Exception("{} doesn't exist in the functions table.".format(function_name))
+
+		else:
+			return self.functions[function_name]
--- variables_table.py
+++ variables_table.py
@@ -4,3 +4,18 @@ from variable import Variable
 class VariablesTable:
 	def __init__(self):
 		self.variables = {}
+
+	def add_variable(self, variable_type, variable_name, variable_value):
+		if variable_name in self.variables:
+			raise Exception("{} was already defined in the variables table".format(variable_name))
+
+		else:
+			var = Variable(variable_type, variable_name, variable_value)
+			self.variables[variable_name] = var
+
+	def get_variable(self, variable_name):
+		if variable_name not in self.variables:
+			raise Exception("{} doesn't exist in the variables table.".format(variable_name))
+		        
+		else:
+			return self.variables[variable_name]
